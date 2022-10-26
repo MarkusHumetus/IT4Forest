84ebfdc846f4ce0583dc7954e8f5877e1c2efec7
Marc Humet <marc.humet.datascience@gmail.com>
2022-10-26 18:53:57 +0200

    python module with image check and treatment

--- img_check.py
+++ img_check.py
@@ -28,3 +28,36 @@ def jpg_files(folder_path):
         if new_list_files[i].split('.')[1] =='jpg':
             list_jpg.append (new_list_files[i])
     return list_jpg
+
+def to_jpg_converter(folder_path, image_name):
+    '''convert images to the jpg format (remove initial format!!)
+        INPUT:  path where the image is and the name of the image 
+        OUTPUT: save image in the same directory and delete image with the original format''' 
+    path_file = os.path.join(folder_path, image_name) 
+    try: 
+        im = Image.open(path_file)
+        new_name= image_name.split('.')[0] +'.jpg'
+        image_new_format = os.path.join(folder_path, new_name)
+        im.save(image_new_format)
+        im.close()
+    except: 
+        print (f'image {image_name} could not be transformed' )
+    remove_file (folder_path, image_name) 
+
+def remove_file (folder_path, name_file):
+    ''' remove file if exists. Else, it shows a warning message
+            INPUT: path where the file is and name of the file with extension (.doc, .rmd, .jpg, ....)
+            OUTPUT: warning message in case any error on name or path or deleting the file if all is fine
+            
+            path ha d'estar amb el format correcte!!
+        '''
+    # Path
+    path_file = os.path.join(folder_path, name_file) 
+    print (path_file)
+    
+    # Remove the file
+    if os.path.exists(path_file):
+        os.remove(path_file)
+        print (name_file + ' deleted!!')
+    else:
+        print ('No file found with such name and route!')
